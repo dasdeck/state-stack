ba9d5c4124c5bdb4bd284b6f13bd0528bb106998
jms <jm@audio.d3ck.net>
2017-11-03 09:53:28 +0100

    semi working state

--- src/state-stack.js
+++ src/state-stack.js
@@ -1,12 +1,14 @@
-/* jshint esversion: 6 */
+
+import Vue from 'vue';
 
 function cloneDeep(data) {
-  return data ? JSON.parse(JSON.stringify(data)) : data;
+    return data ? JSON.parse(JSON.stringify(data)) : data;
 }
 
-function isEqual(a,b) {
-  return JSON.stringify(a) === JSON.stringify(b);
+function isEqual(a, b) {
+    return JSON.stringify(a) === JSON.stringify(b);
 }
+
 /**
  * state based undo/redo class
  * @class
@@ -14,128 +16,222 @@ function isEqual(a,b) {
  */
 export default {
 
-	props: {
-		state: {
-			type: Object,
-			default () { return {} }
-		}
-	},
+    props: ['value'],
+
 	/**
 	 *
 	 * @param {object} [state] - optionally pass the state to manage on construction
 	 */
-	data() {
-		// this.state = state || {}
-		return { stack: null } //this.stack = null
-	},
+    data() {
+		// this.value = state || {}
+        return { stack: null}; //this.stack = null
+    },
 
-	methods: {
-
-		/**
-		 * @private
-		 * @returns {boolean}
-		 */
-		_hasPrevious() {
-			return !!(this.stack && this.stack.prev)
-		},
-
-		/**
-		 *
-		 * @private
-		 * @returns {boolean}
-		 */
-		_isDirty() {
-			return !!(this.stack && !isEqual(this.stack.state, this.getState()))
-		},
-
-		/**
-		 *
-		 * @private
-		 * @returns {boolean}
-		 */
-		_hasNext() {
-			return !!(this.stack && this.stack.next)
-		},
-
-		/**
-		 * gets the current managed state
-		 * @returns {object}
-		 */
-		getState() {
-			return this.state
-		},
-
-		/**
-		 *
-		 * @param state
-		 */
-		setState(state) {
-			this.state = state
-			this.$emit('changed')
-		},
-
-		/**
-		 *
-		 * @returns {boolean}
-		 */
-		canRedo() {
-			return !!(this.stack && this.stack.next && !this._isDirty())
-		},
-
-		/**
-		 *
-		 * @returns {boolean}
-		 */
-		canUndo() {
-			return !!(this._isDirty() || this._hasPrevious())
-		},
-
-		/**
-		 * returns text of redoable action
-		 * @returns {string}
-		 */
-		getRedoText() {
-			if (this._hasNext()) {
-				return this.stack.name
-			}
-		},
+    computed: {
 
 		/**
 		 * returns text of undoable action
 		 * @returns {string}
 		 */
-		getUndoText() {
-			if (this._isDirty()) {
-				return this.stack.name
-			} else if (this._hasPrevious()) {
-				return this.stack.prev.name
-			}
-		},
+        undoText() {
+
+            if (this.canUndo) {
+
+                if (this._isDirty()) {
+                    return this.stack.name;
+                } else if (this._hasPrevious()) {
+                    return this.stack.prev.name;
+                }
+
+            } else {
+
+                return '';
+
+            }
+        },
+
+        /**
+		 *
+		 * @returns {boolean}
+		 */
+        canRedo() {
+            return !!(this.stack && this.stack.next && !this._isDirty());
+        },
+
+		/**
+		 *
+		 * @returns {boolean}
+		 */
+        canUndo() {
+            return !!(this._isDirty() || this._hasPrevious());
+        },
+
+        /**
+		 * returns text of redoable action
+		 * @returns {string}
+		 */
+        redoText() {
+            if (this.canRedo) {
+                return this.stack.name;
+            } else {
+                return '';
+            }
+        },
+        redos() {
+            var res = [];
+
+            var pointer = this.stack;
+
+            if (!pointer) return [];
+
+            while (pointer.next) {
+                pointer = pointer.next;
+                res.push(pointer);
+            }
+            return res;
+        },
+        undos() {
+            var res = [];
+
+            var pointer = this.stack;
+
+            if (!pointer) return [];
+
+            if (this._isDirty()) {
+                res.push(pointer);
+            }
+
+            while (pointer.prev) {
+                pointer = pointer.prev;
+                res.push(pointer);
+            }
+            return res;
+        }
+    },
+
+    methods: {
+
+        clear() {
+            this.stack = null;
+        },
+
+		/**
+		 * @private
+		 * @returns {boolean}
+		 */
+        _hasPrevious() {
+            return !!(this.stack && this.stack.prev);
+        },
+
+		/**
+		 *
+		 * @private
+		 * @returns {boolean}
+		 */
+        _isDirty() {
+            return !!(this.stack && !isEqual(this.stack.state, this.value));
+        },
+
+		/**
+		 *
+		 * @private
+		 * @returns {boolean}
+		 */
+        _hasNext() {
+            return !!(this.stack && this.stack.next);
+        },
 
 		/**
 		 * undo the current transaction
 		 */
-		undo() {
-			if (!this._isDirty() && this._hasPrevious()) {
-				this.stack = this.stack.prev
-			}
-			if (!this.stack.next) {
-				this.stack.next = { prev: this.stack }
-			}
+        undo() {
+            if (!this.canUndo) return;
 
-			this.stack.next.state = cloneDeep(this.getState())
+            if (!this._isDirty() && this._hasPrevious()) {
+                this.stack = this.stack.prev;
+            }
 
-			this.setState(cloneDeep(this.stack.state))
-		},
+            if (!this.stack.next) {
+                Vue.set(this.stack, 'next', { prev: this.stack });
+            }
 
+            this.stack.next.state = cloneDeep(this.value);
+
+            this.value = cloneDeep(this.stack.state);
+
+            this.$emit('changed', this.value);//update v-model
+            this.$emit('input', this.value);//update v-model
+            this.$emit('update:value', this.value);//update value.sync in vue >= 2.2
+        },
+
+        getStart() {
+            if (!this.stack) return false;
+
+            var pointer = this.stack;
+
+            while (pointer.prev) {
+                pointer = pointer.prev;
+            }
+            return pointer;
+        },
+
+        contains(state) {
+            if (!this.stack) return false;
+
+            var pointer = this.getStart();
+
+            while (pointer) {
+                if (pointer === state) {
+                    return true;
+                }
+                pointer = pointer.next;
+            }
+
+            return false;
+
+        },
+
+        jumpToState(stateToLoad) {
+            if (!this.stack) throw 'you can not jump to a state on an empty stack';
+            if (!this.contains(stateToLoad)) throw 'you can only jumpt to local states';
+
+            if (this.pendingRedo) {
+                this.stack.name = this.pendingRedo.name;
+                this.stack.group = this.pendingRedo.group;
+                delete this.pendingRedo;
+            }
+
+            if (this._isDirty()) {
+                if (!this.stack.next) Vue.set(this.stack, 'next', { prev: this.stack });
+                this.stack.next.state = cloneDeep(this.value);
+            }
+
+            this.value = cloneDeep(stateToLoad.state);
+            this.stack = stateToLoad;
+
+            this.$emit('changed', this.value);//update v-model
+            this.$emit('input', this.value);//update v-model
+            this.$emit('update:value', this.value);//update value.sync in vue >= 2.2
+
+        },
+
+        setState(state) {
+            this.value = state;
+        },
+
+        getState() {
+            return this.value;
+        },
 		/**
 		 * redo last undone transaction
-		 * is not save to call if canUdno returns false
+		 * is not save to call if canUndo returns false
 		 */
-		redo() {
-			this.stack = this.stack.next
-			this.setState(cloneDeep(this.stack.state))
-		},
+        redo() {
+            if (!this.canRedo) return;
+
+            this.jumpToState(this.stack.next);
+
+        },
 
 		/**
 		 *
@@ -143,30 +239,45 @@ export default {
 		 * @param {mixed} [group] - optional group to merge
 		 * consecutive chagnes to one transaction
 		 */
-		startTransaction(name, group) {
-			let nextTransaction
+        startTransaction(name, group) {
+            let nextTransaction;
 
-			if (this.stack) {
-				if ((group && this.stack.group === group) || !this._isDirty()) {
-					nextTransaction = this.stack
-				}
-			}
+            if (this.stack) {
 
-			if (!nextTransaction) {
-				nextTransaction = {
-					prev: this.stack
-				}
+                if (group && this.stack.group === group || !this._isDirty()) {
+                    nextTransaction = this.stack;
+                }
 
-				if (this.stack) {
-					this.stack.next = nextTransaction
-				}
+                if (group && this.stack.prev && this.stack.prev.group === group && !this._isDirty()) {
+                    this.stack = this.stack.prev;
+                    nextTransaction = this.stack;
+                }
 
-				this.stack = nextTransaction
-				nextTransaction.state = cloneDeep(this.getState())
-			}
+            }
 
-			nextTransaction.name = name
-			nextTransaction.group = group
-		}
-	}
-}
+            if (this.canRedo && nextTransaction) {
+                this.pendingRedo = {};
+                this.pendingRedo.name = nextTransaction.name;
+                this.pendingRedo.group = nextTransaction.group;
+            }
+
+            if (!nextTransaction) {
+
+                nextTransaction = {
+                    prev: this.stack
+                };
+
+                if (this.stack) {
+                    this.stack.next = nextTransaction;
+                }
+
+                nextTransaction.state = cloneDeep(this.value);
+                this.stack = nextTransaction;// this.stack = nextTransaction;
+
+            }
+
+            this.stack.name = name;
+            this.stack.group = group;
+        }
+    }
+};
--- test/state-stack.test.js
+++ test/state-stack.test.js
@@ -32,12 +32,13 @@ describe('state-stack simple tests', () => {
 
     stateStack.startTransaction('some action')
 
-    expect(stateStack.canUndo()).toBe(false)
-    expect(stateStack.canRedo()).toBe(false)
+    expect(stateStack.canUndo).toBe(false)
+    expect(stateStack.canRedo).toBe(false)
   })
 })
 
 describe('state-stack advanced', () => {
+
   beforeEach(function () {
     this.state = { value: 'initial' }
     this.stateStack = new StateStack()
@@ -48,19 +49,20 @@ describe('state-stack advanced', () => {
   })
 
   it('undo x2', function () {
+    
     this.stateStack.startTransaction('first change')
     this.state.value = 1
 
     this.stateStack.startTransaction('second change')
     this.state.value = 2
 
-    expect(this.stateStack.canRedo()).toBe(false)
-    expect(this.stateStack.canUndo()).toBe(true)
+    expect(this.stateStack.canRedo).toBe(false)
+    expect(this.stateStack.canUndo).toBe(true)
 
     this.stateStack.undo()
 
-    expect(this.stateStack.canRedo()).toBe(true)
-    expect(this.stateStack.canUndo()).toBe(true)
+    expect(this.stateStack.canRedo).toBe(true)
+    expect(this.stateStack.canUndo).toBe(true)
 
     expect(this.state.value).toBe(1)
 
@@ -70,17 +72,19 @@ describe('state-stack advanced', () => {
   })
 
   it('undo do and undo again', function () {
+
+    
     this.stateStack.startTransaction('some action')
 
     this.state.value = 1
 
     this.stateStack.undo()
 
-    this.state.value = 1
+    this.state.value = 2
 
-    expect(this.stateStack.canRedo()).toBe(false)
+    expect(this.stateStack.canRedo).toBe(false)
 
-    expect(this.stateStack.getUndoText()).toBe('some action')
+    expect(this.stateStack.undoText).toBe('some action')
 
     this.stateStack.undo()
 
@@ -91,24 +95,24 @@ describe('state-stack advanced', () => {
     this.stateStack.startTransaction('some unusedAction')
     this.stateStack.startTransaction('some action')
 
-    expect(this.stateStack.canUndo()).toBe(false)
-    expect(this.stateStack.canRedo()).toBe(false)
+    expect(this.stateStack.canUndo).toBe(false)
+    expect(this.stateStack.canRedo).toBe(false)
 
     this.state.value = 'first change'
 
-    expect(this.stateStack.canUndo()).toBe(true)
+    expect(this.stateStack.canUndo).toBe(true)
     expect(this.stateStack._hasPrevious()).toBe(false)
-    expect(this.stateStack.getUndoText()).toBe('some action')
+    expect(this.stateStack.undoText).toBe('some action')
 
     this.stateStack.undo()
 
     expect(this.stateStack._isDirty()).toBe(false)
     expect(this.stateStack._hasPrevious()).toBe(false)
 
-    expect(this.stateStack.getRedoText()).toBe('some action')
+    expect(this.stateStack.redoText).toBe('some action')
 
-    expect(this.stateStack.canUndo()).toBe(false)
-    expect(this.stateStack.canRedo()).toBe(true)
+    expect(this.stateStack.canUndo).toBe(false)
+    expect(this.stateStack.canRedo).toBe(true)
     expect(this.state.value).toBe('initial')
 
     this.stateStack.redo()
@@ -116,14 +120,14 @@ describe('state-stack advanced', () => {
     expect(this.stateStack._isDirty()).toBe(false)
     expect(this.stateStack._hasPrevious()).toBe(true)
 
-    expect(this.stateStack.canUndo()).toBe(true)
-    expect(this.stateStack.canRedo()).toBe(false)
+    expect(this.stateStack.canUndo).toBe(true)
+    expect(this.stateStack.canRedo).toBe(false)
     expect(this.state.value).toBe('first change')
 
     this.stateStack.undo()
 
-    expect(this.stateStack.canUndo()).toBe(false)
-    expect(this.stateStack.canRedo()).toBe(true)
+    expect(this.stateStack.canUndo).toBe(false)
+    expect(this.stateStack.canRedo).toBe(true)
     expect(this.state.value).toBe('initial')
 
     this.state.value = 'another change'
@@ -131,24 +135,40 @@ describe('state-stack advanced', () => {
     expect(this.stateStack._isDirty()).toBe(true)
     expect(this.stateStack._hasPrevious()).toBe(false)
 
-    expect(this.stateStack.canUndo()).toBe(true)
-    expect(this.stateStack.canRedo()).toBe(false)
+    expect(this.stateStack.canUndo).toBe(true)
+    expect(this.stateStack.canRedo).toBe(false)
     expect(this.state.value).toBe('another change')
   })
 
   it('grouping', function () {
+
+    
     let group = {}
-
+    
     this.stateStack.startTransaction('grouped change', group)
-
+    
     this.state.value = 1
-
+    
     this.stateStack.startTransaction('grouped change', group)
-
+    
     this.state.value = 2
-
+    
     this.stateStack.undo()
 
     expect(this.state.value).toBe('initial')
+  })
+
+  it('restart pending redo', function () {
+
+    this.stateStack.startTransaction('operation a');
+    this.state.value = 'a';
+    this.stateStack.undo();
+
+    this.stateStack.startTransaction('operation b');
+    this.stateStack.redo();
+
+    expect(this.stateStack.undoText).toBe('operation a');
+
+
   })
 })
